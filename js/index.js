ba6ffb9efe210e7d7d800e200a311e43d45e86c2
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-02-10 14:31:11 +0100

    section button and paragraph added in js

--- js/index.js
+++ js/index.js
@@ -8,16 +8,21 @@ section.style.height = "537px";
 
 // Create an h1 element
 const header = document.createElement("h1");
-header.textContent = "Header";
+header.textContent = "Find out how we can save your time in Backpacking in Europe";
 
 // Create a p element
 const paragraph = document.createElement("p");
-paragraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+paragraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur. ";
 
 // Create a button element
 const button = document.createElement("button");
-button.textContent = "Click me";
+button.textContent = "Explore";
+button.style.color = "white";
+button.style.fontSize = "18px";
 button.style.backgroundColor = "#F78065";
+button.style.border = "none"
+button.style.width = "250px"
+button.style.height = "65px"
 
 // Append the h1, p and button elements to the section
 section.appendChild(header);
